0573c9ec280b43f1fa2d1e5625fedd441232c3b2
miles <milesward2004@gmail.com>
2021-11-16 14:58:01 -0800

    Added Recipes

---
+++ BreweryProjectTests/RecipeTests.cs
@@ -0,0 +1,72 @@
+using System.Linq;
+using System;
+
+using NUnit.Framework;
+using BreweryProjectEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrewProjectTests 
+{
+    [TestFixture]
+    public class RecipeTests 
+    {
+        BreweryProjectContext dbContext;
+        Recipe theRecipe;
+        List<Recipe> recipes;
+
+        [SetUp]
+        public void Setup()
+        {
+            dbContext = new BreweryProjectContext();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+                List<Recipe> testRecipes = dbContext.Recipes.OrderBy(theList => theList.Name).ToList();
+                //Console.WriteLine("running TearDown function in BatchTests");
+                PrintAll(testRecipes);
+                foreach (Recipe batchInBatches in testRecipes)
+                {
+                    if (batchInBatches.RecipeId > 4)
+                    {
+                        dbContext.Remove(batchInBatches);
+                    }
+                }
+
+                dbContext.SaveChanges();
+                //Console.WriteLine("stopping TearDown function in BatchTests");
+        }
+
+
+
+        [Test]
+        public void GetAllTest()
+        {
+            recipes = dbContext.Recipes.OrderBy(theRecipe => theRecipe.RecipeId).ToList();
+            Assert.AreEqual(4, recipes.Count);
+        }
+
+        [Test]
+        public void testTesting()
+        {
+                Assert.AreEqual(10, 10);
+        }
+
+        [Test]
+        public void GetByPrimaryKeyTest()
+        {
+            theRecipe = dbContext.Recipes.Find(2);
+            Assert.IsNotNull(theRecipe);
+            Assert.AreEqual("Krampus' Special Sauce", theRecipe.Name);
+        }
+
+        public void PrintAll(List<Recipe> recipes)
+        {
+            foreach (Recipe theRecipe in recipes)
+            {
+                Console.WriteLine("Name: {0}, ID: {1}", theRecipe.Name, theRecipe.RecipeId);
+            }
+        }
+    }
+}
