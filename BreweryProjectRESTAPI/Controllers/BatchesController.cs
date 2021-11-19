114f141c43fb8d42a12271b86136fc9f601f4373
miles <milesward2004@gmail.com>
2021-11-19 15:02:17 -0800

    Added a bunch of varies classes.

---
+++ BreweryProjectRESTAPI/Controllers/BatchesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BreweryProjectEFCore.Models;
+
+namespace BreweryProjectRESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatchesController : ControllerBase
+    {
+        private readonly BreweryProjectContext _context;
+
+        public BatchesController(BreweryProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Batches
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Batch>>> GetBatches()
+        {
+            return await _context.Batches.ToListAsync();
+        }
+
+        // GET: api/Batches/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Batch>> GetBatch(int id)
+        {
+            var batch = await _context.Batches.FindAsync(id);
+
+            if (batch == null)
+            {
+                return NotFound();
+            }
+
+            return batch;
+        }
+
+        // PUT: api/Batches/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBatch(int id, Batch batch)
+        {
+            if (id != batch.BatchId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(batch).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BatchExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Batches
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Batch>> PostBatch(Batch batch)
+        {
+            _context.Batches.Add(batch);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBatch", new { id = batch.BatchId }, batch);
+        }
+
+        // DELETE: api/Batches/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBatch(int id)
+        {
+            var batch = await _context.Batches.FindAsync(id);
+            if (batch == null)
+            {
+                return NotFound();
+            }
+
+            _context.Batches.Remove(batch);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BatchExists(int id)
+        {
+            return _context.Batches.Any(e => e.BatchId == id);
+        }
+    }
+}
---
+++ BreweryProjectRESTAPI/Controllers/ProductContainerSizesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BreweryProjectEFCore.Models;
+
+namespace BreweryProjectRESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductContainerSizesController : ControllerBase
+    {
+        private readonly BreweryProjectContext _context;
+
+        public ProductContainerSizesController(BreweryProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductContainerSizes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductContainerSize>>> GetProductContainerSizes()
+        {
+            return await _context.ProductContainerSizes.ToListAsync();
+        }
+
+        // GET: api/ProductContainerSizes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductContainerSize>> GetProductContainerSize(int id)
+        {
+            var productContainerSize = await _context.ProductContainerSizes.FindAsync(id);
+
+            if (productContainerSize == null)
+            {
+                return NotFound();
+            }
+
+            return productContainerSize;
+        }
+
+        // PUT: api/ProductContainerSizes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductContainerSize(int id, ProductContainerSize productContainerSize)
+        {
+            if (id != productContainerSize.ContainerSizeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productContainerSize).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductContainerSizeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductContainerSizes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductContainerSize>> PostProductContainerSize(ProductContainerSize productContainerSize)
+        {
+            _context.ProductContainerSizes.Add(productContainerSize);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductContainerSize", new { id = productContainerSize.ContainerSizeId }, productContainerSize);
+        }
+
+        // DELETE: api/ProductContainerSizes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductContainerSize(int id)
+        {
+            var productContainerSize = await _context.ProductContainerSizes.FindAsync(id);
+            if (productContainerSize == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductContainerSizes.Remove(productContainerSize);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductContainerSizeExists(int id)
+        {
+            return _context.ProductContainerSizes.Any(e => e.ContainerSizeId == id);
+        }
+    }
+}
